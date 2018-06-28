20bbbc5cf768c45b71ef6f0a276e818188aae482
André Schulz <andre.schulz.99@edu.unipar.br>
2018-06-28 14:56:54 -0300

    Add files via upload

--- js/demo/demo.js
+++ js/demo/demo.js
@@ -8,6 +8,16 @@
     function (event) {
       event.preventDefault()
       document.getElementById('result').value = md5(input.value)
+      var copyText = document.getElementById("result");
+
+      /* Select the text field */
+      copyText.select();
+
+      /* Copy the text inside the text field */
+      document.execCommand("copy");
+
+      /* Alert the copied text */
+      
     }
   )
   input.value = ''
